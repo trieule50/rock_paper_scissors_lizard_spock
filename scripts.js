b092fd2d931528c2004db1343d00bd7784fdaf6b
trieule50 <trieule50@gmail.com>
2021-08-26 13:35:31 -0400

    Get all elements

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-console.log('hello')
+const userRock = document.querySelector('.rock');
+const userScissors = document.querySelector('.scissors');
+const userPaper = document.querySelector('.paper');
+const userLizard = document.querySelector('.lizard');
+const userSpock = document.querySelector('.spock');
+
+
+let choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+
+const randomMove = () => {
+    return choices[Math.floor(Math.random()*choices.length)]
+}
+
+let computerMove = randomMove();
+
